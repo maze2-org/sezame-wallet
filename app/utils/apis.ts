defe2920fc1fe41e67475c38e49ed12324f117a6
D.M <bestteamboss@gmail.com>
2022-02-27 23:07:00 -0800

    asset add func and modify dashboard screen

--- app/utils/apis.ts
+++ app/utils/apis.ts
@@ -14,3 +14,15 @@ export const getCoinDetails = (symbol): Promise<CoingeckoCoin> => {
       return error
     })
 }
+
+export const getCoinPrices = (ids) : Promise<Array<any>> => {
+  return axios
+
+    .get(coingeckoBaseUrl + "/coins/markets?vs_currency=usd&ids=" + ids + "&order=market_cap_desc&per_page=10&page=1&sparkline=false")
+    .then(function (response) {
+      return response.data
+    })
+    .catch(function (error) {
+      return error
+    })
+}
