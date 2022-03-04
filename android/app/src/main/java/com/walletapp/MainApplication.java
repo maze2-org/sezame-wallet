e75798831265e98d278141a39f40480f42f9252d
Muhammed <aapozaid@gmail.com>
2022-03-04 08:09:23 +0200

    DEV: adding splash screen refs #4107

--- android/app/src/main/java/com/walletapp/MainApplication.java
+++ android/app/src/main/java/com/walletapp/MainApplication.java
@@ -7,6 +7,7 @@ import androidx.annotation.NonNull;
 
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.reactnativemmkv.MmkvPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
--- app/screens/welcome/welcome-screen.tsx
+++ app/screens/welcome/welcome-screen.tsx
@@ -1,7 +1,9 @@
-import React, { FC } from "react"
+import React, { FC, useEffect } from "react"
 import { View, ViewStyle, TextStyle, ImageStyle, SafeAreaView } from "react-native"
 import { StackScreenProps } from "@react-navigation/stack"
 import { observer } from "mobx-react-lite"
+import SplashScreen from "react-native-splash-screen"
+
 import {
   Button,
   Header,
@@ -54,6 +56,9 @@ const FOOTER_CONTENT: ViewStyle = {
 
 export const WelcomeScreen: FC<StackScreenProps<NavigatorParamList, "welcome">> = observer(
   ({ navigation }) => {
+    useEffect(() => {
+      SplashScreen.hide()
+    }, [])
     return (
       <View testID="WelcomeScreen" style={RootPageStyle}>
         <Screen style={CONTAINER}>
