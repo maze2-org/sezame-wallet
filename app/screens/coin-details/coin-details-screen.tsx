f2464d8d4da1784c634c030e5d27569ee9b28fa7
Aram Suqiasyan <aramsuqiasyan20@gmail.com>
2022-03-21 14:17:03 +0400

    feat: add receive modal

--- app/screens/coin-details/coin-details-screen.tsx
+++ app/screens/coin-details/coin-details-screen.tsx
@@ -1,7 +1,14 @@
-import React, { FC, useEffect, useState } from "react"
+import React, {
+  createRef,
+  FC,
+  useEffect,
+  useRef,
+  useState,
+} from "react"
 import { observer } from "mobx-react-lite"
 import {
   View,
+  Text as TextRn,
   ViewStyle,
   Image,
   ImageStyle,
@@ -9,13 +16,28 @@ import {
   TextStyle,
   ScrollView,
   Linking,
+  Modal,
+  SafeAreaView,
+  TouchableOpacity,
+  Dimensions,
+  Clipboard,
 } from "react-native"
 import { StackNavigationProp, StackScreenProps } from "@react-navigation/stack"
 import FontAwesome5Icon from "react-native-vector-icons/FontAwesome5"
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons"
 import IonIcons from "react-native-vector-icons/Ionicons"
+import copyImg from "../../../assets/svg/copy.svg"
 import { NavigatorParamList } from "../../navigators"
-import { Button, CoinCard, Footer, PriceChart, Screen, Text } from "../../components"
+import FlashMessage, { showMessage } from "react-native-flash-message"
+import {
+  Button,
+  CoinCard,
+  Drawer,
+  Footer,
+  PriceChart,
+  Screen,
+  Text,
+} from "../../components"
 // import { useNavigation } from "@react-navigation/native"
 // import { useStores } from "../../models"
 import { color, spacing } from "../../theme"
@@ -24,7 +46,21 @@ import { getCoinDetails, getMarketChart } from "utils/apis"
 import { CoingeckoCoin } from "types/coingeckoCoin"
 import { useStores } from "models"
 import { BackgroundStyle, CONTAINER, MainBackground, SEPARATOR } from "theme/elements"
+
+import styles
+  from "../send/styles"
+import QRCode
+  from "react-native-qrcode-svg"
+import {
+  SvgXml
+} from "react-native-svg"
+import ready
+  from "../../../assets/svg/ready.svg"
+import {
+  SvgFromXml
+} from "react-native-svg/lib/typescript"
 // import InAppBrowser from "react-native-inappbrowser-reborn"
+const {height} = Dimensions.get("screen")
 
 const ROOT: ViewStyle = {
   backgroundColor: color.palette.black,
@@ -183,9 +219,73 @@ const TRANSACTION_ITEM_DATE: TextStyle = {
   fontSize: 13,
   marginVertical: spacing[1],
 }
+const RECEIVE_MODAL_WRAPPER: ViewStyle  = {
+  alignItems:"center",
+  justifyContent:"center",
+  height:height,
+  backgroundColor: 'rgba(0,0,0,0.3)',
+
+}
+const RECEIVE_MODAL_CONTAINER: ViewStyle  = {
+  width:317,
+  alignItems:"center",
+  borderRadius:8,
+  backgroundColor:color.palette.noise,
+  paddingHorizontal:10
+}
+const QR_CONTAINER: ViewStyle = {
+  display: "flex",
+  alignItems: "center",
+  alignContent: "center",
+  backgroundColor:color.palette.white,
+  padding:20,
+  borderRadius:8,
+  marginBottom:40,
+  marginVertical:20
+}
+const RECEIVE_MODAL_CLOSE_WRAPPER: ViewStyle = {
+  width:"100%",
+  alignItems:"flex-end",
+}
+const RECEIVE_MODAL_CLOSE: ViewStyle = {
+  marginTop:10,
+}
+const RECEIVE_MODAL_COPY_WRAPPER: ViewStyle = {
+  backgroundColor:color.palette.darkblack,
+  width:280,
+  borderRadius:8,
+  alignItems:"center",
+  marginBottom:25,
+
+
+}
+const  RECEIVE_MODAL_ADDRESS: ViewStyle = {
+  padding:20
+}
+const  RECEIVE_MODAL_ADDRESS_TEXT: TextStyle = {
+  textAlign:"center"
+}
+const RECEIVE_MODAL_COPY_BUTTON: ViewStyle = {
+  width:"100%",
+  justifyContent:"center",
+  alignItems:"center",
+  flexDirection:"row",
+  height:40,
+  borderTopColor:color.palette.lineColor,
+  borderTopWidth:1,
+}
+const RECEIVE_MODAL_COPY_TEXT: TextStyle = {
+  fontSize:11,
+}
+const COPY_ICON: ViewStyle = {
+  position:'absolute',
+  left:-40
+}
 
 export const CoinDetailsScreen: FC<StackScreenProps<NavigatorParamList, "coinDetails">> = observer(
   function CoinDetailsScreen({ route }) {
+    const modalFlashRef = useRef<FlashMessage>();
+    const [receiveIsVisible, setReceiveIsVisible] = useState<boolean>(false)
     const [coinData, setCoinData] = useState<CoingeckoCoin | null>(null)
     const [chartData, setChartData] = useState<any[]>([])
     const [chartDays, setChartDays] = useState<number | "max">(1)
@@ -216,7 +316,10 @@ export const CoinDetailsScreen: FC<StackScreenProps<NavigatorParamList, "coinDet
     // Pull in navigation via hook
     const navigation = useNavigation<StackNavigationProp<NavigatorParamList>>()
     const goToSend = () => navigation.navigate("send", { coinId: route.params.coinId })
-    const goToReceive = () => navigation.navigate("receive", { coinId: route.params.coinId })
+    const toggleReceiveModal = (value: boolean) => {
+      // navigation.navigate("receive", { coinId: route.params.coinId })
+      setReceiveIsVisible(value)
+    }
 
     const truncateHash = (hash: string) => {
       return hash.substring(0, 15) + "..." + hash.substring(hash.length - 15, hash.length)
@@ -247,6 +350,23 @@ export const CoinDetailsScreen: FC<StackScreenProps<NavigatorParamList, "coinDet
       //   console.log(error)
       // }
     }
+
+    const copyAddress = () => {
+      Clipboard.setString(asset.address)
+      Clipboard.getString()
+        .then((link)=>{
+          modalFlashRef.current && modalFlashRef.current.showMessage({
+            message: "Copied to clipboard",
+            type: "success",
+          })
+        })
+        .catch((e)=>{
+          console.log(e)
+        })
+
+    }
+
+
     return (
       <Screen style={ROOT} preset="scroll">
         <ImageBackground source={MainBackground} style={BackgroundStyle}>
@@ -267,14 +387,14 @@ export const CoinDetailsScreen: FC<StackScreenProps<NavigatorParamList, "coinDet
                       <FontAwesome5Icon name="arrow-up" size={18} color={color.palette.white} />
                       <Text style={VERTICAL_ICON_BTN_TEXT}>Send</Text>
                     </Button>
-                    <Button style={VERTICAL_ICON_BTN} onPress={goToReceive}>
+                    <Button style={VERTICAL_ICON_BTN} onPress={()=>toggleReceiveModal(true)}>
                       <FontAwesome5Icon name="arrow-down" size={18} color={color.palette.white} />
                       <Text style={VERTICAL_ICON_BTN_TEXT}>Receive</Text>
                     </Button>
                   </View>
                 </View>
                 {!!chartData && !!chartData.length && (
-                  <PriceChart data={chartData.map((p) => p[1])}></PriceChart>
+                  <PriceChart data={chartData.map((p) => p[1])}/>
                 )}
                 <View style={COIN_DETAILS_CONTAINER}>
                   <View style={TIMEFRAME_BTNS}>
@@ -311,13 +431,13 @@ export const CoinDetailsScreen: FC<StackScreenProps<NavigatorParamList, "coinDet
                         <Text style={BALANCE_STAKING_CARD_NOTE}> (~1$)</Text>
                       </View>
 
-                      <View style={SEPARATOR}></View>
+                      <View style={SEPARATOR}/>
                       <Button style={BALANCE_STAKING_CARD_BTN}>
                         <MaterialCommunityIcons
                           style={BALANCE_STAKING_CARD_BTN_ICON}
                           size={18}
                           name="swap-vertical-circle-outline"
-                        ></MaterialCommunityIcons>
+                        />
                         <Text style={BALANCE_STAKING_CARD_BTN_TEXT}>SWAP</Text>
                       </Button>
                     </View>
@@ -327,13 +447,13 @@ export const CoinDetailsScreen: FC<StackScreenProps<NavigatorParamList, "coinDet
                         <Text style={BALANCE_STAKING_CARD_AMOUNT}> 0.459</Text>
                         <Text style={BALANCE_STAKING_CARD_NOTE}>Available rewards 0.02 (~1$)</Text>
                       </View>
-                      <View style={SEPARATOR}></View>
+                      <View style={SEPARATOR}/>
                       <Button style={BALANCE_STAKING_CARD_BTN}>
                         <FontAwesome5Icon
                           size={18}
                           style={BALANCE_STAKING_CARD_BTN_ICON}
                           name="database"
-                        ></FontAwesome5Icon>
+                        />
                         <Text style={BALANCE_STAKING_CARD_BTN_TEXT}>MANAGE STAKING</Text>
                       </Button>
                     </View>
@@ -402,11 +522,69 @@ export const CoinDetailsScreen: FC<StackScreenProps<NavigatorParamList, "coinDet
           showRightButton
           rightButtonText="Explore"
           RightButtonIcon={(props) => (
-            <IonIcons {...props} name="globe-outline" size={23}></IonIcons>
+            <IonIcons {...props} name="globe-outline" size={23} />
           )}
           onRightButtonPress={() => openLink("https://www.blockchain.com/explorer")}
           onLefButtonPress={goBack}
-        ></Footer>
+        />
+
+        <Modal
+          visible={receiveIsVisible}
+          animationType={"fade"}
+          onRequestClose={() => toggleReceiveModal(false)}
+          transparent
+        >
+            <TouchableOpacity style={RECEIVE_MODAL_WRAPPER}
+                              activeOpacity={0}
+                              onPress={()=>toggleReceiveModal(false)}
+            >
+            <View   style={RECEIVE_MODAL_CONTAINER}>
+              <View style={RECEIVE_MODAL_CLOSE_WRAPPER}>
+                <TouchableOpacity
+                  style={RECEIVE_MODAL_CLOSE}
+                  activeOpacity={0.8}
+                  hitSlop={{top:10,left:10,right:10,bottom:10}}
+                  onPress={() => toggleReceiveModal(false)}>
+                  <IonIcons
+                    name={"close-outline"}
+                    size={30}
+                    color={color.palette.white}
+                  />
+                </TouchableOpacity>
+              </View>
+
+              <View style={QR_CONTAINER}>
+                <QRCode value={asset.address} size={185} />
+              </View>
+
+              <View style={RECEIVE_MODAL_COPY_WRAPPER}>
+                <View style={RECEIVE_MODAL_ADDRESS}>
+                  <TextRn style={RECEIVE_MODAL_ADDRESS_TEXT}>
+                    {(asset.address.match(/.{4,5}/g)).map(e => {
+                      return <Text
+                        key={e}>{e} </Text>
+                    })}
+                  </TextRn>
+                </View>
+
+                <TouchableOpacity style={RECEIVE_MODAL_COPY_BUTTON}
+                                  onPress={copyAddress}
+                >
+                  <View>
+                  <SvgXml
+                    stroke={color.palette.gold}
+                    xml={copyImg}
+                    height={20}
+                    style={COPY_ICON}
+                  />
+                  <Text style={RECEIVE_MODAL_COPY_TEXT}>COPY</Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+            </View>
+            </TouchableOpacity>
+          <FlashMessage ref={modalFlashRef} position="bottom" />
+        </Modal>
       </Screen>
     )
   },
