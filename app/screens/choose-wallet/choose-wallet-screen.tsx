b53754df5a4e9f2fb309421b139cadaea4e4516f
Aram Suqiasyan <aramsuqiasyan20@gmail.com>
2022-03-22 18:07:24 +0400

    refs #4173: after locking wallet clear password field

--- app/screens/choose-wallet/choose-wallet-screen.tsx
+++ app/screens/choose-wallet/choose-wallet-screen.tsx
@@ -108,7 +108,7 @@ export const ChooseWalletScreen: FC<
     handleSubmit,
     setValue,
     formState: { errors, isValid },
-  } = useForm({ mode: "onChange" })
+  } = useForm({ mode: "onChange" });
 
   const onSubmit = async (data) => {
     setLoading(true)
@@ -124,6 +124,7 @@ export const ChooseWalletScreen: FC<
     } finally {
       setLoading(false)
     }
+    setValue('walletPassword', '')
   }
 
   return (
